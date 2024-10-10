6dfe4f83350141c43b547c01930ebc73be568630
DewaGedeEkaBudhiarta <dodehebat86@gmail.com>
2024-10-10 09:24:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E_arsip
+ e-arsip project for LLDIKTI VIII
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('content.partials.sidebar');
+});
+
+//tanya dion knp index ku gk bisa jalan
+//tanya dion knp list-file nya gk bisa masuk ke layout app.blade.php
