216399d602a48007c16a2f95b9f8483f78e671b7
DewaGedeEkaBudhiarta <dodehebat86@gmail.com>
2024-10-10 14:47:49 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home.index');
 });
-
-//tanya dion knp index ku gk bisa jalan
-//tanya dion knp list-file nya gk bisa masuk ke layout app.blade.php
