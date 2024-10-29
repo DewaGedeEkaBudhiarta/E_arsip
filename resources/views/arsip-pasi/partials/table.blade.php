8b15593a83861d7ce369d61c146af8aa03182187
Dionisius Adi <dionisiusadiprabupamungkas@gmail.com>
2024-10-29 13:32:48 +0800

    sidebar-update-arsip commit (#13)

--- resources/views/arsip-pasi/partials/table.blade.php
+++ resources/views/arsip-pasi/partials/table.blade.php
@@ -1,37 +1,36 @@
 <div class="container mx-auto p-4">
-  <h1 class="text-3xl font-bold mb-4">Kinerja Bidang Pendidikan</h1>
+    <h1 class="text-3xl font-bold mb-4">Kinerja Bidang Pendidikan</h1>
 
-  <div class="flex justify-between mb-4">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          + Tambah Bidang Pendidikan
-      </button>
-      <a href="#" class="text-blue-500 hover:underline">Home / Kinerja Bidang Pendidikan</a>
-  </div>
+    <div class="flex justify-between mb-4">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            + Tambah Bidang Pendidikan
+        </button>
+        <a href="#" class="text-blue-500 hover:underline">Home / Kinerja Bidang Pendidikan</a>
+    </div>
 
-  <div class="flex items-center mb-4">     
-      <div class="relative inline-block">
-          <button class="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded">
-              Filter
-              <i class="fas fa-caret-down"></i>
-          </button>
-          <ul class="absolute hidden text-gray-700 bg-white rounded shadow-md">
-              <li class="px-4 py-2 hover:bg-gray-100">
-                  <input type="radio" id="filter1" name="filter">
-                  <label for="filter1">Filter 1</label>
-              </li>
-              <li class="px-4 py-2 hover:bg-gray-100">
-                  <input type="radio" id="filter2" name="filter">
-                  <label for="filter2">Filter 2</label>
-              </li>
-              <li class="px-4 py-2 hover:bg-gray-100">
-                  <input type="radio" id="filter3" name="filter">
-                  <label for="filter3">Filter 3</label>
-              </li>
-          </ul>
-      </div>
-      <input type="text" id="search-input" class="ml-2 w-full pl-10 text-sm text-gray-700" placeholder="Cari Bidang Pendidikan">
-  </div>
-
+    <div class="flex items-center mb-4">
+        <div class="relative inline-block">
+            <button class="bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold py-2 px-4 rounded">
+                Filter
+                <i class="fas fa-caret-down"></i>
+            </button>
+            <ul class="absolute hidden text-gray-700 bg-white rounded shadow-md">
+                <li class="px-4 py-2 hover:bg-gray-100">
+                    <input type="radio" id="filter1" name="filter">
+                    <label for="filter1">Arsip Terbuka</label>
+                </li>
+                <li class="px-4 py-2 hover:bg-gray-100">
+                    <input type="radio" id="filter2" name="filter">
+                    <label for="filter2">Arsip Tertutup</label>
+                </li>
+                <li class="px-4 py-2 hover:bg-gray-100">
+                    <input type="radio" id="filter3" name="filter">
+                    <label for="filter3">Arsip Rahasia</label>
+                </li>
+            </ul>
+        </div>
+        <input type="text" id="search-input" class="ml-2 w-full pl-10 text-sm text-gray-700" placeholder="Cari Bidang Pendidikan">
+    </div>
   <table class="w-full text-sm text-left text-gray-500">
       <thead class="text-xs text-gray-700 uppercase bg-gray-50">
           <tr>
