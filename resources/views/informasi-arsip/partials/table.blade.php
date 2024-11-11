5a02706f4d6144ca3263a91bdc13af32f4d7e8a6
Dionisius Adi <dionisiusadiprabupamungkas@gmail.com>
2024-11-11 08:55:49 +0800

    klasifikasi_update

--- resources/views/informasi-arsip/partials/table.blade.php
+++ resources/views/informasi-arsip/partials/table.blade.php
@@ -4,22 +4,14 @@
     <table class="w-full text-sm text-left text-gray-500">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
             <tr>
-                <th class="py-3 px-6">Kode Klasifikasi</th>
-                <th class="py-3 px-6">Nama Klasifikasi</th>
-                <th class="py-3 px-6">Deskripsi Klasifikasi</th>
+                <th class="py-3 px-6">Fungsi</th>
+                <th class="py-3 px-6">Primer</th>
+                <th class="py-3 px-6">Kegiatan</th>
+                <th class="py-3 px-6">Sekunder</th>
+                <th class="py-3 px-6">Transaksi</th>
+                <th class="py-3 px-6">Tersier</th>
+                <th class="py-3 px-6">Indeks</th>
             </tr>
         </thead>
-        <tbody>
-            <tr class="bg-white border-b">
-                <td class="py-4 px-6">PR.00.00</td>
-                <td class="py-4 px-6">Rencana Pembangunan Jangka Panjang (RPJP)</td>
-                <td class="py-4 px-6">Naskah yang berkaitan dengan Rencana Pembangunan Jangka Panjang (RPJP) </td>
-            </tr>
-            <tr class="bg-white border-b">
-                <td class="py-4 px-6">PR.00.01 </td>
-                <td class="py-4 px-6">Rencana Pembangunan Jangka Menengah (RPJMN) </td>
-                <td class="py-4 px-6">Naskah yang berkaitan dengan Rencana Pembangunan Jangka Menengah (RPJMN) </td>
-            </tr>
-        </tbody>
     </table>
 </div>
