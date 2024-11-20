d9740383ecd581adb465c1dac7bbbd07ba5d78f3
Dionisius Adi <dionisiusadiprabupamungkas@gmail.com>
2024-11-20 15:32:55 +0800

    update-home

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,23 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/', function () {
         return view('home.index');
     });
-        
+
+    Route::get('/rekap', function () {
+        return view('rekapitulasi.index');
+    });
+
     Route::get('/informasi-arsip', [KlasifikasiArsipController::class, 'index'])->name('informasi-arsip.index');
     Route::post('/informasi-arsip', [KlasifikasiArsipController::class, 'store'])->name('informasi-arsip.store');
     Route::delete('/informasi-arsip/{id}', [KlasifikasiArsipController::class, 'destroy'])->name('informasi-arsip.destroy');
-    
+
     Route::get('/pemindahan/table-active', function () {
         $files = DB::table('files')->where('status', 'active')->get();
         return view('pemindahan-arsip.index', ['partial' => 'table-active', 'files' => $files]);
-    });    
+    });
     Route::get('/pemindahan/table-inactive', function () {
         $files = DB::table('files')->where('status', 'inactive')->get();
         return view('pemindahan-arsip.index', ['partial' => 'table-inactive', 'files' => $files]);
-    });    
+    });
     Route::post('/files/{id}/update-status', [FileController::class, 'updateStatus'])->name('files.update-status');
 
     Route::get('/arsip-pasi', [FileController::class, 'index'])->name('arsip-pasi.index');
