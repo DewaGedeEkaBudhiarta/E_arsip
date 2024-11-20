3a37180dcf54f42221d9dac3233bcc447cefb7b7
Dionisius Adi <dionisiusadiprabupamungkas@gmail.com>
2024-11-20 13:02:06 +0800

    ui commit (#19)

--- resources/views/pemindahan-arsip/partials/table-inactive.blade.php
+++ resources/views/pemindahan-arsip/partials/table-inactive.blade.php
@@ -1,37 +1,30 @@
 <div class="container mx-auto p-4">
-    <h1 class="text-3xl font-bold mb-4">Kinerja Bidang Pendidikan</h1>
+    <h1 class="text-3xl font-bold mb-4">Daftar Arsip Inaktif</h1>
 
-    <div class="flex justify-between mb-4">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            + Tambah Bidang Pendidikan
-        </button>
-        <a href="#" class="text-blue-500 hover:underline">Home / Kinerja Bidang Pendidikan</a>
+    <div class="flex items-center mb-4">
+        <input type="text" id="search-input" class="ml-2 w-full pl-10 text-sm text-gray-700" placeholder="Cari Arsip Inaktif">
     </div>
-
-    <div class="flex items-center mb-4">        
-        <input type="text" id="search-input" class="ml-2 w-full pl-10 text-sm text-gray-700" placeholder="Cari Bidang Pendidikan">
-    </div>
-  <table class="w-full text-sm text-left text-gray-500">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-          <tr>
-              <th class="py-3 px-6">Kode Klasifikasi</th>
-              <th class="py-3 px-6">No Berkas</th>
-              <th class="py-3 px-6">Nama</th>              
-              <th class="py-3 px-9">Kurun Waktu</th>
-              <th class="py-3 px-6">Indeks(Kata tangkap/Kata Kunci)</th>
-              <th class="py-3 px-6">Keterangan</th>
-              <th class="py-3 px-10">classification</th>
-              <th class="py-3 px-10">Aksi</th>
-          </tr>
-      </thead>
-      <tbody>
+    <table class="w-full text-sm text-left text-gray-500">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+            <tr>
+                <th class="py-3 px-6">Kode Klasifikasi</th>
+                <th class="py-3 px-6">No Berkas</th>
+                <th class="py-3 px-6">Nama</th>
+                <th class="py-3 px-9">Kurun Waktu</th>
+                <th class="py-3 px-6">Indeks(Kata tangkap/Kata Kunci)</th>
+                <th class="py-3 px-6">Keterangan</th>
+                <th class="py-3 px-10">Klasifikasi</th>
+                <th class="py-3 px-10">Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
             {{-- Display uploaded files --}}
             @forelse ($files as $file)
             <tr class="bg-white border-b">
-                @if (Auth::user()->role == 'admin' || 
-                    (Auth::user()->role == 'user' && $file->classification == 'umum') || 
-                    (Auth::user()->role == 'user' && $file->user_id == Auth::id()) || 
-                    (Auth::user()->role == 'user' && DB::table('file_user')->where('file_id', $file->id)->where('user_id', Auth::id())->exists()))
+                @if (Auth::user()->role == 'admin' ||
+                (Auth::user()->role == 'user' && $file->classification == 'umum') ||
+                (Auth::user()->role == 'user' && $file->user_id == Auth::id()) ||
+                (Auth::user()->role == 'user' && DB::table('file_user')->where('file_id', $file->id)->where('user_id', Auth::id())->exists()))
                 <td class="py-4 px-6">{{ $file->kode_klasifikasi }}</td>
                 <td class="py-4 px-6">{{ $file->no_berkas }}</td>
                 <td class="py-4 px-6">{{ $file->file_name }}</td>
@@ -44,8 +37,11 @@
                         @csrf
                         <input type="hidden" name="status" value="active">
                         <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-0.5 px-1 rounded">
-                            Set Active
+                            Set Aktif
                         </button>
+                        <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0.5 px-1 rounded">
+                            Usul Musnah
+                        </a>
                     </form>
                     <form action="{{ route('delete', ['id' => $file->id]) }}" method="POST" style="display:inline;">
                         @csrf
@@ -62,9 +58,9 @@
             </tr>
             @empty
             <tr>
-                    <td class="py-4 px-6 text-center" colspan="7">No files found</td>
+                <td class="py-4 px-6 text-center" colspan="7">No files found</td>
             </tr>
             @endforelse
-      </tbody>      
-  </table>
-</div> 
+        </tbody>
+    </table>
+</div>
